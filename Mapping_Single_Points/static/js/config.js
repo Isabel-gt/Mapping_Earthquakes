94f18bb967535429e4dea5579c4dc1e25828b7ba
isabel <isagtomasini@gmail.com>
2022-12-08 16:26:12 -0600

    Adding all the files

--- Mapping_Single_Points/static/js/config.js
+++ Mapping_Single_Points/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiaXNhZ3QiLCJhIjoiY2xiYjdzNG8xMDJrNTQwcXc5aDhxbHFoaCJ9.fiVpeeMNYDxreGSHk_CqWg";
+const API_KEY = "pk.eyJ1IjoiaXNhZ3QiLCJhIjoiY2xiZm42MnE3MDFlbDNvcDd4Zm9nZGJhaSJ9.T3xxdWRdbOH1onCG1oST9Q";
